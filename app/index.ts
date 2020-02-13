b8ea23a7ba1e9bfa1b12c649162196dd7dc117e1
denis180889 <denis.pob@gmail.com>
2020-02-13 16:19:08 +0200

    Initial commit

---
+++ app/index.ts
@@ -0,0 +1,50 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import MongoClient from './mongoClient';
+import multer from 'multer';
+import { ObjectId } from 'mongodb';
+
+const app = express();
+
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
+
+const jsonParser = bodyParser.json()
+
+const mongoClient = new MongoClient('mongodb://localhost:27017/user_database');
+
+export interface User {
+    _id?: ObjectId;
+    nick: string;
+    name: string;
+    age: string;
+    about: string;
+    avatar: string;
+}
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.post('/create-user', jsonParser, async function (req: any, res: any) {
+    const user: User = req.body;
+
+    await mongoClient.insertObject('users', user);
+
+    res.sendStatus(201);
+});
+
+app.post('/addAvatar/:userNick', upload.single('avatar'), async function (req: any, res: any) {
+    const base64Avatar = req.file.buffer.toString('base64');
+
+    const user: User | null = await mongoClient.findObject<User>('users', { nick: req.params.userNick });
+    if (user) user.avatar = base64Avatar;
+
+    if (user) await mongoClient.updateObject<User>('users', { nick: req.params.userNick }, user);
+
+    res.sendStatus(201);
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
---
+++ app/mongoClient.ts
@@ -0,0 +1,53 @@
+
+import Client from 'mongodb';
+
+export default class MongoClient {
+
+    url: string;
+
+    constructor(dbUrl: string) {
+        this.url = dbUrl;
+    }
+
+    async getConnection() {
+        return await Client.connect(this.url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+    }
+
+    async insertObject(collection: string, obj: object) {
+        const client = await this.getConnection();
+        try {
+            const db = client.db();
+            await db.collection(collection).insertOne(obj);
+            console.log("Done, user was saved");
+        }
+        finally {
+            await client.close();
+        }
+    }
+
+    async findObject<T>(collection: string, filter: any): Promise<T | null> {
+        const client = await this.getConnection();
+        try {
+            const db = client.db();
+            const result = await db.collection<T>(collection).findOne(filter);
+            return result;
+        }
+        finally {
+            await client.close();
+        }
+    }
+
+    async updateObject<T>(collection: string, filter: any, obj: T) {
+        const client = await this.getConnection();
+        try {
+            const db = client.db();
+            await db.collection<T>(collection).update(filter, obj);
+        }
+        finally {
+            await client.close();
+        }
+    }
+}
