a1700a47a3ec3d615776974e53bda2da394168c9
denis180889 <denis.pob@gmail.com>
2020-02-23 15:45:08 +0200

    Split project on back and front parts

---
+++ front/components/App.jsx
@@ -0,0 +1,11 @@
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Welcome to Instaclone app</h2>
+
+                <button>Login</button>
+            </div>
+        );
+    }
+}
